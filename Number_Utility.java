524fc46235a004a9df678e83446d4bde77c9024b
Dhairya-jain-15 <Dhairyajain1512@gmail.com>
2025-12-28 16:36:26 +0530

    added armstrong

--- Number_Utility.java
+++ Number_Utility.java
@@ -3,49 +3,70 @@ package JAVACODES;
 // Palindrome number
 
 import java.util.Scanner;
-
+import java.lang.Math;
 // Armstrong number
 // Factorial & Fibonacci
+
 public class Number_Utility {
-        static void isprime(int num){
-            if(num==1){
-                System.out.println("neither prime not composite");
-            }
-            for(int i=2;i<num;i++){
-                if (num%i==0) {
-                    System.out.println("not Prime because it is disvible by " + i );
-                    return;
-                }
-            }
-                System.out.println("prime");
+
+    static void isprime(int num) {
+        if (num == 1) {
+            System.out.println("neither prime not composite");
         }
-        static void ispalindrome(int pal){
-            int og=pal;
-            int ans=0;
-            for (int i=0;pal>0;i++) {
-                int temp=pal%10;
-                ans=temp+ans*10;
-                pal=pal/10;  
-            }
-            if(ans==og){
-                System.out.println("palindrome");
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.println("not Prime because it is disvible by " + i);
                 return;
             }
-            else{
-                System.out.println("Not palindrome");
-            }
         }
-        static void armstrong(){
-            
+        System.out.println("prime");
+    }
+
+    static void ispalindrome(int pal) {
+        int og = pal;
+        int ans = 0;
+        for (int i = 0; pal > 0; i++) {
+            int temp = pal % 10;
+            ans = temp + ans * 10;
+            pal = pal / 10;
         }
-        static void factorial(){
-            
+        if (ans == og) {
+            System.out.println("palindrome");
+            return;
+        } else {
+            System.out.println("Not palindrome");
         }
-        static void fabonacci(){
-            
+    }
+
+    static void armstrong(int num) {
+        int count = 0, total = 0, c = num;
+        while (c > 0) {
+            c = c / 10;
+            count++;
         }
+        c = num;
+        while (c>0) {
+            int digit = c % 10;
+            total = total + (int) Math.pow(digit, count);
+            c = c / 10;
+        }
+        if (total == num) {
+            System.out.println("Armstrong number");
+        } else {
+            System.out.println("Not an Armstrong number");
+        }
+    }
+
+    static void factorial() {
+
+    }
+
+    static void fabonacci() {
+
+    }
+
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("which operation you want to do:");
         System.out.println("1. check prime");
         System.out.println("2. palindrome");
@@ -53,20 +74,22 @@ public class Number_Utility {
         System.out.println("4. factorial");
         System.out.println("5. fabonacci");
         System.out.print("Enter your choice: ");
-        int option=s.nextInt();
+        int option = s.nextInt();
         switch (option) {
             case 1:
                 System.out.print("enter the number: ");
-                int prime=s.nextInt();
+                int prime = s.nextInt();
                 isprime(prime);
                 break;
             case 2:
                 System.out.print("enter the number: ");
-                int palindrome=s.nextInt();
+                int palindrome = s.nextInt();
                 ispalindrome(palindrome);
                 break;
             case 3:
-                armstrong();
+                System.out.print("enter the number: ");
+                int num = s.nextInt();
+                armstrong(num);
                 break;
             case 4:
                 factorial();
