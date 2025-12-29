6ad95e31fbc6cdbc3c837588ece5f260abf5b885
Dhairya-jain-15 <Dhairyajain1512@gmail.com>
2025-12-30 00:56:48 +0530

    COMPLETED ATM

--- ATM_Simulation.java
+++ ATM_Simulation.java
@@ -8,20 +8,28 @@ public class ATM_Simulation {
         d_amt = d_amt + currbal;
         return d_amt;
     }
-
-    static void withdraw() {
+    static int checkbalance(int currbal) {
+        return currbal;
     }
-
-    static void checkbalance() {
+    static int withdraw(int w_amt,int currbal) {
+        if(w_amt>currbal){
+            System.out.println("NO MONEY");
+            return currbal;
+        }
+        else{
+            currbal=currbal-w_amt;
+            System.out.println("Withdrawl Completed");
+            return currbal;
+        }
     }
-
     static void exit() {
+        System.out.println("THANK YOU!");
+        System.out.println("======================================================");
         return;
     }
 
-    static void display() {
+    static void display(int currbal) {
         Scanner s = new Scanner(System.in);
-        int currbal = 0;
         System.out.println("======================================================");
         System.out.println("ATM SIMULATION");
         System.out.println("1. Deposit");
@@ -34,14 +42,23 @@ public class ATM_Simulation {
         switch (Choice) {
             case 1:
                 int d_amt = 0;
+                System.out.print("Enter amount want to deposit: ");
                 d_amt = s.nextInt();
                 currbal = deposit(d_amt, currbal);
-                System.out.println(currbal);
-                main(null);
+                System.out.println("Deposited");
+                display(currbal);
                 break;
             case 2:
+                int res=checkbalance(currbal);
+                System.out.println("The Current balance is: " + res);
+                display(currbal);
                 break;
             case 3:
+                int w_amt = 0;
+                System.out.print("Enter amount want to withdraw: ");
+                w_amt = s.nextInt();
+                currbal=withdraw(w_amt,currbal);
+                display(currbal);
                 break;
             case 4:
                 exit();
@@ -52,6 +69,8 @@ public class ATM_Simulation {
     }
 
     public static void main(String[] args) {
-        display();
+        Scanner s = new Scanner(System.in);
+        int currbal = 0;
+        display(currbal);
     }
 }
