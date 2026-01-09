cbe896369184ef68a764d6b947640da2442b3447
Dhairya-jain-15 <Dhairyajain1512@gmail.com>
2026-01-09 14:12:20 +0530

    M

--- Employee_management.java
+++ Employee_management.java
@@ -10,7 +10,17 @@ public class Employee_management {
     public void addEmployee(int id, String name, int sal) {
         empList.add(new Employee(id, name, sal));
     }
+    public int find_id(int id){
+        for (int idx = 0; idx < empList.size(); idx++) {
+            if(empList.get(idx).emp_id == id) {
+                return idx;
+            }
+        }
+        return -1;
+    }
+    public void update_sal(){
 
+    }
     public void displayEmployees() {
         for (Employee e : empList) {
             System.out.println(e.emp_id + " " + e.emp_name + " " + e.emp_sal);
