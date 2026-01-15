b8a8438922691cc917828ef959de143a8b6eba2c
Dhairya-jain-15 <Dhairyajain1512@gmail.com>
2026-01-15 20:10:17 +0530

    edited  bank account system

--- Bank_account_system.java
+++ Bank_account_system.java
@@ -1,6 +1,16 @@
 package JAVACODES;
 
 public class Bank_account_system{
+    public class account{
+
+    }
+    public class saving_account extends account{
+
+    }
+    public class current_account extends account{
+
+    }
+
     public static void main(String[] args) {
         
     }
